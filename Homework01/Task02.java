58e85ec378bca997a0ed66f37e08e4dd0f478d06
yury-poloshkov <1286600@google.com>
2023-06-07 01:45:13 +0300

    task02 completed

--- Homework01/Task02.java
+++ Homework01/Task02.java
@@ -3,6 +3,27 @@
  * Вывести все простые числа от 1 до 1000
  */
 public class Task02 {
-
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[J");
+        int target = 1000;
+        int columns = 0;
+        for (int i = 2; i <= target; i++) {
+            if (IsSimple(i)) {
+                System.out.print(i + "\t");
+                columns++;
+                if (columns % 10 == 0) System.out.println();
+            }
+        }
+        System.out.println();
+    }
+    public static boolean IsSimple(int number) {
+        boolean flag = true;
+        int curNumber = 2;
+        while (flag == true && (curNumber <= Math.sqrt(number))){
+            if (number %  curNumber == 0 ) flag = false;
+            curNumber++;
+        }
+        return flag;
+    }
     
 }
