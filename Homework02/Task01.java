1af5a024d793c9c07107d214c53c4cd111a07fe6
yury-poloshkov <1286600@google.com>
2023-06-10 17:08:07 +0300

    task02 completed

---
+++ Homework02/Task01.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/*
+ * Task01
+ * Дана строка sql-запроса "select * from students WHERE ". 
+ * Сформируйте часть WHERE этого запроса, используя StringBuilder. 
+ * Данные для фильтрации приведены ниже в виде json-строки.
+ * Если значение null, то параметр не должен попадать в запрос.
+ * Пример данной строки {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+ * Вывод: select * from students WHERE name=Ivanov AND country=Russia AND city=Moscow
+ */
+public class Task01 {
+
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[J");
+        String jsonString = null;
+        try {
+            BufferedReader freader = new BufferedReader(new FileReader("jsonstringtask01.txt")); 
+            jsonString = freader.readLine();
+            freader.close();
+        } catch (Exception e) {
+            System.out.println("Файл данных отсутствует...");
+        }
+
+        
+        System.out.println(jsonString);
+
+    }
+}
---
+++ Homework02/Task02.java
@@ -0,0 +1,49 @@
+/*
+ * Task02
+ * Реализуйте алгоритм сортировки пузырьком числового массива, 
+ * результат после каждой итерации запишите в лог-файл.
+ */
+
+import java.io.FileWriter;
+import java.util.Arrays;
+
+public class Task02 {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[J");
+        int arraySize = 10;
+        int minValue = 0;
+        int maxValue = 100;
+        int[] testArray = initializeIntArray(arraySize, minValue, maxValue);
+        System.out.println(Arrays.toString(testArray));
+        bubbleSort(testArray);
+        System.out.println(Arrays.toString(testArray));
+    } 
+
+    public static int[] initializeIntArray(int size, int min, int max){
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (min + Math.random()*(max-min));             
+        }
+        return array;
+    }
+
+    public static void bubbleSort(int[] array){
+        try {
+            FileWriter logfile = new FileWriter("bubblesortlog.txt", false);
+            logfile.append(Arrays.toString(array) + "\n");
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = 0; j < array.length -1 - i; j++) {
+                    if (array[j] > array[j + 1]){
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j+1] = temp;
+                        logfile.append(Arrays.toString(array) + "\n");
+                    }
+                }
+            }
+            logfile.close();
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+}
